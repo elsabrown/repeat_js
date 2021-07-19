4a6f2b506d84efa2b18dbc599b2cb8d8b0747a8a
Maria Averchenko <miss.elsa.brown@gmail.com>
2021-07-19 16:39:04 +0300

    js_2 done

--- tasks.js
+++ tasks.js
@@ -42,22 +42,102 @@ let users = [
   { id: 12, name: "Денис", age: 20 },
 ];
 
-let usersAfterYear = users.map((user) => {
-		user.age += 1;
-		return user;
-	}).filter(user => user.age >= 18);
+let usersAfterYear = users
+  .map((user) => {
+    user.age += 1;
+    return user;
+  })
+  .filter((user) => user.age >= 18);
 
-	console.log(usersAfterYear);
+console.log(usersAfterYear);
 
 /* 3. На основании массива usersAfterYear, полученного на основании предыдущего задания создать новый массив usersAfterYearUniqueAge, в котором нет пользователей с одинаковым возрастом. Если у вас есть два пользователя ({id: 2, name: "Петя", age: 19} и {id: 4, name: "Света", age: 19}, то вы должны оставить только Петю или Свету одного а не их двоих). (Это задание похоже на первое задания про массив arr и arrUnique)*/
 
+let users = [
+  { id: 1, name: "Вася", age: 19 },
+  { id: 2, name: "Петя", age: 18 },
+  { id: 3, name: "Маша", age: 17 },
+  { id: 4, name: "Света", age: 18 },
+  { id: 5, name: "Наташа", age: 17 },
+  { id: 6, name: "Женя", age: 12 },
+  { id: 7, name: "Коля", age: 30 },
+  { id: 8, name: "Настя", age: 16 },
+  { id: 9, name: "Антон", age: 19 },
+  { id: 10, name: "Иван", age: 20 },
+  { id: 11, name: "Павел", age: 16 },
+  { id: 12, name: "Денис", age: 20 },
+];
+
+let usersAfterYear = users
+  .map((user) => {
+    user.age += 1;
+    return user;
+  })
+  .filter((user) => user.age >= 18);
+
+console.log(usersAfterYear);
+
+let usersAfterYearUniqueAge = usersAfterYear.reduce((users, user) => {
+  //здесь должно быть что-то
+  if {
+    users.push(user);
+  }
+  return users;
+}, []);
+
+console.log(usersAfterYearUniqueAge);
+
 /* 4. Посчитать первые 8 чисел Фибоначчи использую рекурсию. Например: fibonachi(n); (При вызове функции положите n = 8); */
+
+let fibonachi = (n) => {
+  return n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
+};
+console.log(fibonachi(8));
 
 /* 5. Посчитать с помощью рекурсии факториал числа 10. Например: factorial(n); (При выозове полжите n = 10); */
 
-/* 6. Напишите функцию func() которая при каждом своем вызове будет возвращать количество своих вызовов (сколько раз ее вызызвали до этого плюс этот вызов) Используйте замыкание;*/
+function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+}
+
+console.log(factorial(10));
+
+/* 6. Напишите функцию func(), которая при каждом своем вызове будет возвращать количество своих вызовов (сколько раз ее вызызвали до этого плюс этот вызов) Используйте замыкание;*/
+
+function a() {
+  let i = 1;
+  return function () {
+    return i++;
+  };
+}
+
+let func = a();
+
+console.log(func());
+console.log(func());
+console.log(func());
 
 /* 7. Создайте функцию-конструктор, которая позволяет создавать объект пользователя с полями first_name, second_name, age, city.
 	  С помощью данной функции создайте два объекта пользователей (два пользователя), имя, фамилию, возраст, город проживания задайте сами. */
 
+function User(first_name, second_name, age, city) {
+  this.first_name = first_name;
+  this.second_name = second_name;
+  this.age = age;
+  this.city = city;
+}
+
+let mike = new User("Mike", "Brown", "22", "LA");
+let nadya = new User("Nadya", "Adamoyska", "21", "Gdansk");
+
+console.log(mike);
+console.log(nadya.city);
+
 /* 8. Создайте функцию, которая позовляет вывести полную информацию о каждом из двух пользователей созданных в предыдущем задании. Функция должна выполнять вывод информации и не принимать аргументов. По сути нужно одну функцию привязать два объекта.*/
+
+User.prototype.info = function () {
+  console.log(this);
+};
+
+mike.info();
+nadya.info();
