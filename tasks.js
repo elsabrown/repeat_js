7a26b8b6109d4e0603e813ac455be9f32e1aaf4f
Maria Averchenko <miss.elsa.brown@gmail.com>
2021-07-22 21:56:43 +0300

    -

--- tasks.js
+++ tasks.js
@@ -2,18 +2,6 @@
 	Для этого создайте цепочку промисов. Только после того как уборка комнаты завершена, пользователь может начать убирать новую. Если одна из комнат оказалась слишком грязной, то пекращаем уборку полностью. Для этого функция cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) { ... } должна принимать три аргемента, каждый из кторых описывает 
 	уровень загрязнения каждой комнаты.*/
 
-function cleanRoom(dirtyLevel, dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
-  return new Promise(function (resolve, reject) {
-    if (dirtyLevel < 10) {
-      setTimeout(() => resolve(dirtyLevel_1), dirtyLevel * 1000);
-      setTimeout(() => resolve(dirtyLevel_2), dirtyLevel * 1000);
-      setTimeout(() => resolve(dirtyLevel_3), dirtyLevel * 1000);
-    } else {
-      setTimeout(() => reject("Ошибка"), dirtyLevel);
-    }
-  });
-}
-
 function cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
   return new Promise(function (resolve, reject) {
     if (dirtyLevel_1 < 10 && dirtyLevel_2 < 10 && dirtyLevel_3 < 10) {
