63b1033dd37872fac3862ac0607ada4b68302380
Maria Averchenko <miss.elsa.brown@gmail.com>
2021-08-05 15:18:30 +0300

    promise all

--- tasks.js
+++ tasks.js
@@ -7,8 +7,8 @@ function cleanRoom(dirtyLevel) {
     if (dirtyLevel < 10) {
       setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
     } else {
-      setTimeout(
-        () => reject("Поздно, мы потеряли эту комнату. Здесь слишком грзно")
+      setTimeout(() =>
+        reject("Поздно, мы потеряли эту комнату. Здесь слишком грзно")
       );
     }
   });
@@ -36,45 +36,55 @@ cleanRooms(3, 2, 7);
 	то пекращаем уборку полностью. Для этого функция cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) { ... } 
 	должна принимать три аргемента, каждый из кторых описывает уровень загрязнения каждой комнаты.*/
 
-  function cleanRoom(dirtyLevel) {
-    return new Promise(function (resolve, reject) {
-      if (dirtyLevel < 10) {
-        setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
-      } else {
-        setTimeout(
-          () => reject("Поздно, мы потеряли эту комнату. Здесь слишком грзно")
-        );
-      }
-    });
-  }
+function cleanRoom(dirtyLevel) {
+  return new Promise(function (resolve, reject) {
+    if (dirtyLevel < 10) {
+      setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+    } else {
+      setTimeout(() =>
+        reject("Поздно, здесь слишком грзно")
+      );
+    }
+  });
+}
 
-  function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
- if (dirtyLevel_1 < 10 && dirtyLevel_2 < 10 && dirtyLevel_3 < 10) {
-   cleanRoom(dirtyLevel_1)
-    .then((result) => {
-      return console.log(
-        `Уборка комнаты №1 проведена успешно за ${result} секунд`
-      );
-    })
-    .catch((error) => console.error(error));
-  cleanRoom(dirtyLevel_2)
-    .then((result) => {
-      return console.log(
-        `Уборка комнаты №2 проведена успешно за ${result} секунд`
-      );
-    })
-    .catch((error) => console.error(error));
-  cleanRoom(dirtyLevel_3)
-    .then((result) => {
-      return console.log(
-        `Уборка комнаты №3 проведена успешно за ${result} секунд`
-      );
-    })
-    .catch((error) => console.error(error));
-  } else {
-    console.log('Здесь слишком грязно');
-  }
+function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+  return Promise.all([
+    cleanRoom(dirtyLevel_1),
+    cleanRoom(dirtyLevel_2),
+    cleanRoom(dirtyLevel_3),
+  ]).then((result) =>
+    console.log(`Уборка комнат проведена успешно за ${result} секунд каждая`)
+  );
 }
 
 cleanRooms(3, 11, 4);
 cleanRooms(3, 1, 4);
+
+//   function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+//  if (dirtyLevel_1 < 10 && dirtyLevel_2 < 10 && dirtyLevel_3 < 10) {
+//    cleanRoom(dirtyLevel_1)
+//     .then((result) => {
+//       return console.log(
+//         `Уборка комнаты №1 проведена успешно за ${result} секунд`
+//       );
+//     })
+//     .catch((error) => console.error(error));
+//   cleanRoom(dirtyLevel_2)
+//     .then((result) => {
+//       return console.log(
+//         `Уборка комнаты №2 проведена успешно за ${result} секунд`
+//       );
+//     })
+//     .catch((error) => console.error(error));
+//   cleanRoom(dirtyLevel_3)
+//     .then((result) => {
+//       return console.log(
+//         `Уборка комнаты №3 проведена успешно за ${result} секунд`
+//       );
+//     })
+//     .catch((error) => console.error(error));
+//   } else {
+//     console.log('Здесь слишком грязно');
+//   }
+// }
