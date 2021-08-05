d66247416e3f1359b1b2dd5a6dc2b74ffeea3e2a
Maria Averchenko <miss.elsa.brown@gmail.com>
2021-08-05 21:56:54 +0300

    done

--- tasks.js
+++ tasks.js
@@ -1,5 +1,7 @@
 /* 1. Перепишите цепочку промисов из предыдущего задания, используя async/await*/
 
+
+﻿
 function cleanRoom(dirtyLevel) {
   return new Promise(function (resolve, reject) {
     if (dirtyLevel < 10) {
@@ -12,20 +14,20 @@ function cleanRoom(dirtyLevel) {
   });
 }
 
-function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
-  cleanRoom(dirtyLevel_1)
-    .then((result) => {
-      console.log(`Уборка комнаты №1 проведена успешно за ${result} секунд`);
-      return cleanRoom(dirtyLevel_2);
-    })
-    .then((result) => {
-      console.log(`Уборка комнаты №2 проведена успешно за ${result} секунд`);
-      return cleanRoom(dirtyLevel_3);
-    })
-    .then((result) =>
-      console.log(`Уборка комнаты №3 проведена успешно за ${result} секунд`)
-    )
-    .catch((error) => console.error(error));
+async function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+try {
+let r1 = await cleanRoom(dirtyLevel_1);
+      console.log(`Уборка комнаты №1 проведена успешно за ${dirtyLevel_1} секунд`);
+
+let r2 = await cleanRoom(dirtyLevel_2);
+      console.log(`Уборка комнаты №2 проведена успешно за ${dirtyLevel_2} секунд`);
+
+let r3 = await cleanRoom(dirtyLevel_3);
+      console.log(`Уборка комнаты №3 проведена успешно за ${dirtyLevel_3} секунд`);
+}catch(err) {
+    console.error("Error:", err);
+ }
 }
 
 cleanRooms(3, 2, 7);
+cleanRooms(3, 2, 11);
